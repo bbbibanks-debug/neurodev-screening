c08d0dd1aa5140b09f9f0546d1fd7032fe3181f7
bbbibanks-debug <bbbi.banks@gmail.com>
2026-06-08 22:21:59 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,28 @@ document
 .getElementById("app")
 .innerHTML =
 "<h2>Sistema carregado</h2>";
+
+
+async function iniciar(){
+
+const resposta =
+await fetch(
+"data/catalog.json"
+);
+
+const catalogo =
+await resposta.json();
+
+document
+.getElementById("app")
+.innerHTML =
+
+`
+<h2>
+${catalogo.transtornos[0].nome}
+</h2>
+`;
+
+}
+
+iniciar();
